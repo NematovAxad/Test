7dfd3fbf2cc897b3ce328c0de7939e99effcd259
Ahadjon Nematov <axad.nem4tov@gmail.com>
2022-02-22 11:10:07 +0500

    rank det fix

--- AdminHandler/Handlers/Ranking/RankingQueryHandler.cs
+++ AdminHandler/Handlers/Ranking/RankingQueryHandler.cs
@@ -2,6 +2,7 @@
 using AdminHandler.Results.Ranking;
 using Domain.Enums;
 using Domain.Models;
+using Domain.States;
 using JohaRepository;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -18,36 +19,73 @@ namespace AdminHandler.Handlers.Ranking
     {
         private readonly IRepository<Organizations, int> _organization;
         private readonly IRepository<RankTable, int> _rankTable;
+        private readonly IRepository<XRankTable, int> _xRankTable;
+        private readonly IRepository<GRankTable, int> _gRankTable;
 
-        public RankingQueryHandler(IRepository<Organizations, int> organization, IRepository<RankTable, int> rankTable)
+        public RankingQueryHandler(IRepository<Organizations, int> organization, IRepository<RankTable, int> rankTable, IRepository<XRankTable, int> xRankTable, IRepository<GRankTable, int> gRankTable)
         {
             _organization = organization;
             _rankTable = rankTable;
+            _gRankTable = gRankTable;
+            _xRankTable = xRankTable;
         }
 
         public async Task<RankingQueryResult> Handle(RankingQuery request, CancellationToken cancellationToken)
         {
             RankingQueryResult result = new RankingQueryResult();
 
-            var rank = _rankTable.GetAll();
-            if(request.OrganizationId != 0)
+            var org = _organization.Find(o => o.Id == request.OrganizationId).FirstOrDefault();
+            if (org == null)
+                throw ErrorStates.NotFound("organization " + request.OrganizationId.ToString());
+
+            if(org.OrgCategory == OrgCategory.FarmOrganizations)
             {
-                rank = rank.Where(r => r.OrganizationId == request.OrganizationId);
+                var xRank = _xRankTable.GetAll();
+
+                if (request.OrganizationId != 0)
+                {
+                    xRank = xRank.Where(r => r.OrganizationId == request.OrganizationId);
+                }
+                if (request.Year != 0)
+                {
+                    xRank = xRank.Where(r => r.Year == request.Year);
+                }
+                if (request.Quarter != 0)
+                {
+                    xRank = xRank.Where(r => r.Quarter == request.Quarter);
+                }
+                if (request.FieldId != 0)
+                {
+                    xRank = xRank.Where(r => r.FieldId == request.FieldId);
+                }
+                result.Count = xRank.Count();
+                result.Data = xRank.OrderBy(u => u.Id).ToList<object>();
             }
-            if(request.Year != 0)
+            if (org.OrgCategory == OrgCategory.GovernmentOrganizations)
             {
-                rank = rank.Where(r => r.Year == request.Year);
+                var gRank = _xRankTable.GetAll();
+
+                if (request.OrganizationId != 0)
+                {
+                    gRank = gRank.Where(r => r.OrganizationId == request.OrganizationId);
+                }
+                if (request.Year != 0)
+                {
+                    gRank = gRank.Where(r => r.Year == request.Year);
+                }
+                if (request.Quarter != 0)
+                {
+                    gRank = gRank.Where(r => r.Quarter == request.Quarter);
+                }
+                if (request.FieldId != 0)
+                {
+                    gRank = gRank.Where(r => r.FieldId == request.FieldId);
+                }
+                result.Count = gRank.Count();
+                result.Data = gRank.OrderBy(u => u.Id).ToList<object>();
             }
-            if(request.Quarter!=0)
-            {
-                rank = rank.Where(r => r.Quarter == request.Quarter);
-            }
-            if (request.FieldId != 0)
-            {
-                rank = rank.Where(r => r.FieldId == request.FieldId);
-            }
-            result.Count = rank.Count();
-            result.Data = rank.OrderBy(u => u.Id).ToList<object>();
+
+            
             return result;
         }
     }
