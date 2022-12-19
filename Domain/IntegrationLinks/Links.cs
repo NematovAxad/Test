01e5c9032d7f971b70b6075447a8cf5f4ae2ba89
Ahadjon Nematov <axad.nem4tov@gmail.com>
2022-12-19 18:00:38 +0500

    open data link change

--- Domain/IntegrationLinks/Links.cs
+++ Domain/IntegrationLinks/Links.cs
@@ -6,7 +6,7 @@ namespace Domain.IntegrationLinks
 {
     public static class Links
     {
-        public static string OpenDataurl => "http://172.16.60.9:9006/apiPartner/Partner/NisUzApi";
+        public static string OpenDataurl => "https://data.egov.uz/apiPartner/Partner/NisUzApi";
         public static string ReesterFirstLink => "https://reestr.uz/api/apiProject/Integration/nisgetorgprojects";
         public static string ReesterSecondLink => "https://reestr.uz/api/apiProject/Integration/getproject";
     }
--- Domain/OpenDataModels/OpenDataResultModel.cs
+++ Domain/OpenDataModels/OpenDataResultModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,20 +7,34 @@ namespace Domain.OpenDataModels
 {
     public class OpenDataResultModel
     {
+        [JsonProperty(PropertyName = "result")]
         public ResultClass Result { get; set; }
     }
     public class ResultClass
     {
+        [JsonProperty(PropertyName = "count")]
         public int Count { get; set; }
+        [JsonProperty(PropertyName = "data")]
         public List<Data> Data { get; set; }
     }
     public class Data
     {
+        [JsonProperty(PropertyName = "dataName")]
         public string DataName { get; set; }
+
+        [JsonProperty(PropertyName = "orgName")]
         public string OrgName { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "updateDate")]
         public DateTime UpdateDate { get; set; }
+
+        [JsonProperty(PropertyName = "status")]
         public int Status { get; set; }
+
+        [JsonProperty(PropertyName = "link")]
         public string Link { get; set; }
     }
 }
