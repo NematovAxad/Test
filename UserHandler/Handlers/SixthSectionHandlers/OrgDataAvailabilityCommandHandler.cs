b25b873822a584a98009ece79e7acdaa11179b59
Ahadjon Nematov <axad.nem4tov@gmail.com>
2023-05-26 10:58:18 +0500

    6.4 org data availability crud date check fixed

--- UserHandler/Handlers/SixthSectionHandlers/OrgDataAvailabilityCommandHandler.cs
+++ UserHandler/Handlers/SixthSectionHandlers/OrgDataAvailabilityCommandHandler.cs
@@ -66,7 +66,7 @@ namespace UserHandler.Handlers.SixthSectionHandlers
             if (!model.UserPermissions.Any(p => p == Permissions.SITE_CONTENT_FILLER) && !(model.UserPermissions.Any(p => p == Permissions.OPERATOR_RIGHTS)))
                 throw ErrorStates.Error(UIErrors.UserPermissionsNotAllowed);
 
-            if (deadline.SixthSectionDeadlineDate < DateTime.Now)
+            if (deadline.OperatorDeadlineDate < DateTime.Now)
                 throw ErrorStates.Error(UIErrors.DeadlineExpired);
 
             if (!Links.Sections.Contains(model.Section))
@@ -140,7 +140,7 @@ namespace UserHandler.Handlers.SixthSectionHandlers
             if (!model.UserPermissions.Any(p => p == Permissions.SITE_CONTENT_FILLER) && !(model.UserPermissions.Any(p => p == Permissions.OPERATOR_RIGHTS)))
                 throw ErrorStates.Error(UIErrors.UserPermissionsNotAllowed);
 
-            if (deadline.SixthSectionDeadlineDate < DateTime.Now)
+            if (deadline.OperatorDeadlineDate < DateTime.Now)
                 throw ErrorStates.Error(UIErrors.DeadlineExpired);
 
 
