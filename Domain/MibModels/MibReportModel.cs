3ecc54edb7a6f8af92c17bfeffdc820f176c8c4c
Ahadjon Nematov <axad.nem4tov@gmail.com>
2023-05-31 19:16:25 +0500

    mib report integrated to database done (model added)

---
+++ Domain/MibModels/MibReportModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain.MibModels
+{
+    public class MibReportModel
+    {
+        [JsonProperty(PropertyName = "api_name")]
+        public string ApiName { get; set; }
+
+        [JsonProperty(PropertyName = "owner_id")]
+        public string OwnerInn { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string ApiDescription { get; set; }
+
+        [JsonProperty(PropertyName = "api_version")]
+        public string ApiVersion { get; set; }
+
+        [JsonProperty(PropertyName = "success_count")]
+        public int SuccessCount { get; set; }
+
+        [JsonProperty(PropertyName = "fail_count")]
+        public int FailCount { get; set; }
+
+        [JsonProperty(PropertyName = "overall")]
+        public string Overall { get; set; }
+
+        [JsonProperty(PropertyName = "success_share")]
+        public double SuccessShare { get; set; }
+    }
+}
