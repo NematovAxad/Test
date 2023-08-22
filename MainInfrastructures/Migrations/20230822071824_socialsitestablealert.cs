eb2131dc3ce6be3b5ac9aed04701f856b74b8ae4
Ahadjon Nematov <axad.nem4tov@gmail.com>
2023-08-22 15:18:52 +0500

    migrations added

---
+++ MainInfrastructures/Migrations/20230822071824_socialsitestablealert.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MainInfrastructures.Migrations
+{
+    public partial class socialsitestablealert : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "pool_comment_expert",
+                schema: "organizations",
+                table: "organization_socials",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "pool_except_expert",
+                schema: "organizations",
+                table: "organization_socials",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "pool_comment_expert",
+                schema: "organizations",
+                table: "organization_socials");
+
+            migrationBuilder.DropColumn(
+                name: "pool_except_expert",
+                schema: "organizations",
+                table: "organization_socials");
+        }
+    }
+}
