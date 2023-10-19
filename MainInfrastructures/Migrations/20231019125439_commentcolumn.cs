a2109ebcb151b63e2da8fe46a9050d8ca562f256
Ahadjon Nematov <axad.nem4tov@gmail.com>
2023-10-19 17:56:29 +0500

    exception comment added

---
+++ MainInfrastructures/Migrations/20231019125439_commentcolumn.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MainInfrastructures.Migrations
+{
+    public partial class commentcolumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "comment",
+                schema: "reestrprojects",
+                table: "reestr_project_exception",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "comment",
+                schema: "reestrprojects",
+                table: "reestr_project_exception");
+        }
+    }
+}
